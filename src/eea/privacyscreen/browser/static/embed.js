305af028ae5f8499162355b81b4f7254f027105e
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2023-01-26 21:34:56 +0200

    linting, refs #160818

--- src/eea/privacyscreen/browser/static/embed.js
+++ src/eea/privacyscreen/browser/static/embed.js
@@ -9,23 +9,23 @@ CookiesHelper.createCookie = function(name, domain, days) {
   if (days) {
     var date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = ";expires=" + date.toGMTString();
-  } else expires = "";
+    expires = ';expires=' + date.toGMTString();
+  } else expires = '';
 
   domain = false;
   if (domain) {
     if (document.location.host.indexOf(domain) === -1) {
       domain = document.location.host;
     }
-    domain = ";domain=" + domain;
+    domain = ';domain=' + domain;
   } else domain = '';
 
-  var cookie = name + "=true" + domain + expires + ";path=/";
+  var cookie = name + '=true' + domain + expires + ';path=/';
   document.cookie = cookie;
 };
 
 CookiesHelper.eraseCookie = function(name) {
-  CookiesHelper.createCookie(name, "", -1);
+  CookiesHelper.createCookie(name, '', -1);
 };
 
 function onSubmit(event) {
